d871aad0ec7189953161b4e9f6f055ba984c3c53
João Vitor <71740709+Joao24820@users.noreply.github.com>
2022-01-21 22:20:33 -0300

    Create tabela.py

---
+++ tabela.py
@@ -0,0 +1,14 @@
+print('================================= \n| COD |     PRODUTO     | VALOR  | \n| 100 | Cachorro quente |R$ 8.00 | \n'
+      '| 101 | Hamburguer      |R$ 25.00|\n| 102 | Suco            |R$ 3.00 |\n| 103 | Refrigerante 2l |R$ 6.00 | \n'
+      '| 104 | Salgado         |R$ 4.00 |\n| 105 | Pizza           |R$ 35.00| \n| 106 | Batata Frita    |R$ 14.00|\n'
+      '==================================\nFINALIZAR PEDIDO DIGITE 0')
+
+menu = {
+
+    '100': ['Cachorro quente', 8.00],
+    '101': ['Hamburguer', 25.00],
+    '102': ['Suco', 3.00],
+    '103': ['Refrigerente 2L', 6.00],
+    '104': ['Salgado', 4.00],
+    '105': ['Pizza', 35.00],
+    '106': ['Batata Frita', 14.00]

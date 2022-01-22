2057cad1c536e81fb214abfeb39a7981b69f5a97
João Vitor <71740709+Joao24820@users.noreply.github.com>
2022-01-21 22:20:01 -0300

    Create Pagamento.py

---
+++ Pagamento.py
@@ -0,0 +1,21 @@
+print('\nESCOLHA A FORMA DE PAGAMENTO !')
+print('[1] á vista dinheiro \n[2]Cartão de Debito \n[3]Picpay\n[4]Cartão de Credito')
+
+op = int(input('\nQual a opção acima escolhida: '))
+
+if op == 1:
+    print('Pagamento Confirmado !!')
+if op == 2:
+    print('Pagamento no Cartão de Debito...')
+    sleep(2)
+    print('Pagamento Confirmado !!')
+if op == 3:
+    print('Pagamento Picpay...')
+    sleep(2)
+    print('Pagamento Confirmado !!')
+if op == 4:
+    print('Pagamento Cartão De Credito...')
+    sleep(2)
+    print('Pagamento Confirmado !!')
+
+print('\nOBRIGADO E VOLTE SEMPRE !!')
